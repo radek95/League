36e10473f71f5b266bfd65680d7b56526fd477b9
rutko <rutko@192.168.0.122>
2017-12-29 21:51:04 +0100

    JaRutkowski Adding Git label into League class
    Adding lblHelloGit into League class.

--- src/main/java/com/javafee/league/League.java
+++ src/main/java/com/javafee/league/League.java
@@ -40,6 +40,7 @@ public class League extends JFrame {
 	ArrayList<Team> teams = new ArrayList<Team>();
 	String text1 = null;
 	private JButton btnSaveTeams;
+	private JLabel lblHelloGit;
 
 	/**
 	 * Launch the application.
@@ -106,17 +107,25 @@ public class League extends JFrame {
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[] { 0, 148, 164, 139, 0 };
-		gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gbl_contentPane.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 		gbl_contentPane.columnWeights = new double[] { 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
-		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
+		gbl_contentPane.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
 		contentPane.setLayout(gbl_contentPane);
+		
+		lblHelloGit = new JLabel("Czym byłoby zycie bez GIT'a?");
+		GridBagConstraints gbc_lblHelloGit = new GridBagConstraints();
+		gbc_lblHelloGit.gridwidth = 3;
+		gbc_lblHelloGit.insets = new Insets(0, 0, 5, 5);
+		gbc_lblHelloGit.gridx = 1;
+		gbc_lblHelloGit.gridy = 0;
+		contentPane.add(lblHelloGit, gbc_lblHelloGit);
 
 		JLabel lbTeam1 = new JLabel("Team 1");
 		GridBagConstraints gbc_lbTeam1 = new GridBagConstraints();
 		gbc_lbTeam1.insets = new Insets(0, 0, 5, 5);
 		gbc_lbTeam1.anchor = GridBagConstraints.EAST;
 		gbc_lbTeam1.gridx = 0;
-		gbc_lbTeam1.gridy = 0;
+		gbc_lbTeam1.gridy = 1;
 		contentPane.add(lbTeam1, gbc_lbTeam1);
 
 		textField = new JTextField();
@@ -125,7 +134,7 @@ public class League extends JFrame {
 		gbc_textField.insets = new Insets(0, 0, 5, 0);
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField.gridx = 1;
-		gbc_textField.gridy = 0;
+		gbc_textField.gridy = 1;
 		contentPane.add(textField, gbc_textField);
 		textField.setColumns(10);
 
@@ -134,7 +143,7 @@ public class League extends JFrame {
 		gbc_lblTeam.insets = new Insets(0, 0, 5, 5);
 		gbc_lblTeam.anchor = GridBagConstraints.EAST;
 		gbc_lblTeam.gridx = 0;
-		gbc_lblTeam.gridy = 1;
+		gbc_lblTeam.gridy = 2;
 		contentPane.add(lblTeam, gbc_lblTeam);
 
 		textField_1 = new JTextField();
@@ -143,7 +152,7 @@ public class League extends JFrame {
 		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_1.gridx = 1;
-		gbc_textField_1.gridy = 1;
+		gbc_textField_1.gridy = 2;
 		contentPane.add(textField_1, gbc_textField_1);
 		textField_1.setColumns(10);
 
@@ -152,7 +161,7 @@ public class League extends JFrame {
 		gbc_lblTeam_1.insets = new Insets(0, 0, 5, 5);
 		gbc_lblTeam_1.anchor = GridBagConstraints.EAST;
 		gbc_lblTeam_1.gridx = 0;
-		gbc_lblTeam_1.gridy = 2;
+		gbc_lblTeam_1.gridy = 3;
 		contentPane.add(lblTeam_1, gbc_lblTeam_1);
 
 		textField_2 = new JTextField();
@@ -161,7 +170,7 @@ public class League extends JFrame {
 		gbc_textField_2.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_2.gridx = 1;
-		gbc_textField_2.gridy = 2;
+		gbc_textField_2.gridy = 3;
 		contentPane.add(textField_2, gbc_textField_2);
 		textField_2.setColumns(10);
 
@@ -170,7 +179,7 @@ public class League extends JFrame {
 		gbc_lblTeam_2.insets = new Insets(0, 0, 5, 5);
 		gbc_lblTeam_2.anchor = GridBagConstraints.EAST;
 		gbc_lblTeam_2.gridx = 0;
-		gbc_lblTeam_2.gridy = 3;
+		gbc_lblTeam_2.gridy = 4;
 		contentPane.add(lblTeam_2, gbc_lblTeam_2);
 
 		textField_3 = new JTextField();
@@ -179,7 +188,7 @@ public class League extends JFrame {
 		gbc_textField_3.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_3.gridx = 1;
-		gbc_textField_3.gridy = 3;
+		gbc_textField_3.gridy = 4;
 		contentPane.add(textField_3, gbc_textField_3);
 		textField_3.setColumns(10);
 
@@ -188,7 +197,7 @@ public class League extends JFrame {
 		gbc_lblTeam_3.insets = new Insets(0, 0, 5, 5);
 		gbc_lblTeam_3.anchor = GridBagConstraints.EAST;
 		gbc_lblTeam_3.gridx = 0;
-		gbc_lblTeam_3.gridy = 4;
+		gbc_lblTeam_3.gridy = 5;
 		contentPane.add(lblTeam_3, gbc_lblTeam_3);
 
 		textField_4 = new JTextField();
@@ -197,7 +206,7 @@ public class League extends JFrame {
 		gbc_textField_4.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_4.gridx = 1;
-		gbc_textField_4.gridy = 4;
+		gbc_textField_4.gridy = 5;
 		contentPane.add(textField_4, gbc_textField_4);
 		textField_4.setColumns(10);
 
@@ -206,7 +215,7 @@ public class League extends JFrame {
 		gbc_lblTeam_4.insets = new Insets(0, 0, 5, 5);
 		gbc_lblTeam_4.anchor = GridBagConstraints.EAST;
 		gbc_lblTeam_4.gridx = 0;
-		gbc_lblTeam_4.gridy = 5;
+		gbc_lblTeam_4.gridy = 6;
 		contentPane.add(lblTeam_4, gbc_lblTeam_4);
 
 		textField_5 = new JTextField();
@@ -215,7 +224,7 @@ public class League extends JFrame {
 		gbc_textField_5.insets = new Insets(0, 0, 5, 0);
 		gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_5.gridx = 1;
-		gbc_textField_5.gridy = 5;
+		gbc_textField_5.gridy = 6;
 		contentPane.add(textField_5, gbc_textField_5);
 		textField_5.setColumns(10);
 
@@ -223,7 +232,7 @@ public class League extends JFrame {
 		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
 		gbc_btnNewButton.insets = new Insets(0, 0, 5, 5);
 		gbc_btnNewButton.gridx = 1;
-		gbc_btnNewButton.gridy = 6;
+		gbc_btnNewButton.gridy = 7;
 		contentPane.add(btnNewButton, gbc_btnNewButton);
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -240,14 +249,14 @@ public class League extends JFrame {
 		GridBagConstraints gbc_btnSaveTeams = new GridBagConstraints();
 		gbc_btnSaveTeams.insets = new Insets(0, 0, 5, 5);
 		gbc_btnSaveTeams.gridx = 2;
-		gbc_btnSaveTeams.gridy = 6;
+		gbc_btnSaveTeams.gridy = 7;
 		contentPane.add(btnSaveTeams, gbc_btnSaveTeams);
 
 		btnGenerateCalendar = new JButton("Show the calendar");
 		GridBagConstraints gbc_btnGenerateCalendar = new GridBagConstraints();
 		gbc_btnGenerateCalendar.insets = new Insets(0, 0, 5, 0);
 		gbc_btnGenerateCalendar.gridx = 3;
-		gbc_btnGenerateCalendar.gridy = 6;
+		gbc_btnGenerateCalendar.gridy = 7;
 		contentPane.add(btnGenerateCalendar, gbc_btnGenerateCalendar);
 		btnGenerateCalendar.addActionListener(new ActionListener() {
 
@@ -264,7 +273,7 @@ public class League extends JFrame {
 		gbc_editorPane.gridwidth = 4;
 		gbc_editorPane.fill = GridBagConstraints.BOTH;
 		gbc_editorPane.gridx = 0;
-		gbc_editorPane.gridy = 7;
+		gbc_editorPane.gridy = 8;
 		contentPane.add(editorPane, gbc_editorPane);
 	}
 
